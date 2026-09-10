58f973177dfc37b5713668ae4ce66abf446c0769
huahao <huahao@huahaodeMacBook-Air.local>
2026-09-10 09:41:16 +0800

    chore: use domestic package mirrors on server

--- scripts/bootstrap_server.sh
+++ scripts/bootstrap_server.sh
@@ -10,10 +10,14 @@ if [[ ! -f /.dockerenv ]]; then
 fi
 
 if [[ ! -x .conda-env/bin/python ]]; then
-  conda create --yes --prefix "$project_root/.conda-env" python=3.10 pip
+  conda create --yes --prefix "$project_root/.conda-env" \
+    --override-channels \
+    --channel https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main \
+    python=3.10 pip
 fi
 
-.conda-env/bin/python -m pip install --upgrade pip
-.conda-env/bin/python -m pip install -r requirements.txt
+pip_mirror="https://mirrors.aliyun.com/pypi/simple"
+.conda-env/bin/python -m pip install --index-url "$pip_mirror" --upgrade pip
+.conda-env/bin/python -m pip install --index-url "$pip_mirror" -r requirements.txt
 
 echo "Environment ready: $project_root/.conda-env"
